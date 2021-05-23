593e49c27c137855b81e1ca30f869cc405cd270d
gytanzo <gytanzo@gmail.com>
2021-05-23 19:48:15 -0400

    #3 The code now uses Bot instead of Client

--- Workout.py
+++ Workout.py
@@ -1,34 +1,27 @@
 import os
 import discord
-import random
 from dotenv import load_dotenv
+from discord.ext import commands
+
 
 load_dotenv()
 TOKEN = os.getenv('DISCORD_TOKEN')
 
 intents = discord.Intents.default()
 intents.members = True
+bot = commands.Bot(command_prefix='!', intents=intents)
 
-client = discord.Client(intents=intents)
 
-@client.event
+@bot.event
 async def on_member_join(member):
     # DM the user when they join the server. Expand on this functionality later. 
     await member.send(f'Hi {member.name}, welcome to the Discord Workout server! This is a work in progress, check back later.')
 
-@client.event
-async def on_message(message):
-    # Check to make sure that this isn't the bot itself sending a message. 
-    if message.author == client.user:
-        return
-
+@bot.command(name="register")
+async def register(ctx):
     # This should check for what channel the user is in, we don't want them to be able to register inside the #help channel.
     # For now, I want to be able to use this inside of #deleteme, so I'll move on. 
-    registration_message = "In the future, this will make the bot give a user the \"Registered\" role and then DM the user additional instructions. For now, it is defunct."
+    response = "In the future, this will make the bot give a user the \"Registered\" role and then DM the user additional instructions. For now, it is defunct."
+    await ctx.send(response)
 
-    # Send the message. 
-    if message.content.lower() in ("register".lower(), "register ".lower()): # Ignore capitalization, some users may be on mobile where it would capitalize the word for them. Also account for a space. 
-        response = registration_message
-        await message.channel.send(response)
-   
-client.run(TOKEN)
+bot.run(TOKEN)
