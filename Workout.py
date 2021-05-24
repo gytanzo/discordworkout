52ba3bf6ed7b6e0f67e7c8b4706fce8241d1bf7c
gytanzo <gytanzo@gmail.com>
2021-05-23 20:25:54 -0400

    #4 Added comments to my code so I'll know what I was doing in a few days.

--- Workout.py
+++ Workout.py
@@ -14,24 +14,30 @@ bot = commands.Bot(command_prefix='!', intents=intents)
 
 @bot.event
 async def on_member_join(member):
-    # DM the user when they join the server. Expand on this functionality later. 
+    # When the user joins, give them a "Not Registered" role to limit their access to the first channel. 
     role = discord.utils.get(member.guild.roles, name='Not Registered')
     await member.add_roles(role)
-    await member.send(f'Hi {member.name}, welcome to the Discord Workout server! This is a work in progress, check back later.')
 
 @bot.command(name="join")
 async def join(ctx):
-    # This should check for what channel the user is in, we don't want them to be able to register inside the #help channel.
-    # For now, I want to be able to use this inside of #deleteme, so I'll move on.
-    member = ctx.message.author
-    await ctx.message.delete()
+    # If someone is trying to do this in the wrong channel, don't do anything. 
+    if (ctx.message.channel.id != 844327265533165590):
+        return
 
+    # Delete the message. 
+
+    # Find the member that sent it for registration purposes. 
+    member = ctx.message.author
+
+    # Remove their "Not Registered" role.
     role = discord.utils.get(member.guild.roles, name='Not Registered')
     await member.remove_roles(role)
 
+    # Add a "Registered" role. 
     role = discord.utils.get(member.guild.roles, name='Registered')
     await member.add_roles(role)
 
+    # DM the user the next steps. 
     await member.send("You have been registered. The rest of the registration process will take place through DMs.")
     await member.send("If you have any questions, either now or in the future, please ask them in #help.")
     await member.send("To begin... PLACEHOLDER TEXT")
