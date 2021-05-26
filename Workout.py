e9b2a8db6773b699a184471c0305baac6229e1c8
gytanzo <gytanzo@gmail.com>
2021-05-26 16:15:58 -0400

    #7 Added functionality to both increase and decrease the users current numbers.

--- Workout.py
+++ Workout.py
@@ -21,17 +21,20 @@ async def on_member_join(member):
     role = discord.utils.get(member.guild.roles, name='Not Registered')
     await member.add_roles(role)
 
+# Register with the program. Necessary to use the bot to begin with. 
 @bot.command(name="join")
 async def join(ctx):
     # If someone is trying to do this in the wrong channel, don't do anything. 
     if (ctx.message.channel.id != 844327265533165590):
         return
 
-    # Delete the message. 
-    await ctx.message.delete()
+    message = ctx.message
+    channel = ctx.channel
+    member = message.author
+    channel_id = channel.id
 
-    # Find the member that sent it for registration purposes. 
-    member = ctx.message.author
+    # Delete the message. 
+    await message.delete()
 
     # Remove their "Not Registered" role.
     role = discord.utils.get(member.guild.roles, name='Not Registered')
@@ -40,10 +43,6 @@ async def join(ctx):
     # Add a "Registered" role. 
     role = discord.utils.get(member.guild.roles, name='Registered')
     await member.add_roles(role)
-
-    message = ctx.message
-    member = message.author
-    id = message.channel.id
 
     # Checks if the user sent a valid unit of measurement.
     def check_unit(m):
@@ -105,7 +104,7 @@ async def join(ctx):
         return val
 
     # Create a text file using the channel ID. In other words, the channel ID is the users account number!
-    filename = "Users/" + str(id) + ".txt"
+    filename = "Users/" + str(channel_id) + ".txt"
 
     # Open the file that will store the information provided by the user.
     f = open(filename, "w")
@@ -115,6 +114,7 @@ async def join(ctx):
     await member.send("If you have any questions, either now or in the future, please ask them in #help.")
     await member.send("DISCLAIMER: As of right now, this bot only sends you plans for nSuns 5/3/1 workout. In the future, I may add more programs, but for now this is all I've got.") 
     await member.send("ANOTHER DISCLAIMER: I'm going to be asking you for a lot of information to set up your account. Please respond in exactly the format I ask, I won't recognize it otherwise.")
+    await member.send("ONE LAST DISCLAIMER: I am **not** a licensed fitness trainer or instructor. Use this application at your own risk, I am not liable for any harm that may or may not come to any persons.")
 
     response = ""
 
@@ -143,8 +143,8 @@ async def join(ctx):
     await member.send("To begin, what is your deadlift 1RM?")
     msg = await bot.wait_for('message', check=check_number)
     while(msg.content.lower() != "yes"):
-        response = msg.content
-        await member.send("You answered \"" + msg.content + "\". Is this correct?")
+        response = str(int(msg.content))
+        await member.send("You answered \"" + response + "\". Is this correct?")
         msg = await bot.wait_for('message', check=check_confirmation)
         if (msg.content.lower() == "no"):
             await member.send("You responded no. Please respond with your deadlift 1RM.")
@@ -157,8 +157,8 @@ async def join(ctx):
     await member.send("Cool, thanks. Next, mind telling me your overhead press 1RM?")
     msg = await bot.wait_for('message', check=check_number)
     while(msg.content.lower() != "yes"):
-        response = msg.content
-        await member.send("You answered \"" + msg.content + "\". Is this correct?")
+        response = str(int(msg.content))
+        await member.send("You answered \"" + response + "\". Is this correct?")
         msg = await bot.wait_for('message', check=check_confirmation)
         if (msg.content.lower() == "no"):
             await member.send("You responded no. Please respond with your overhead press 1RM.")
@@ -171,8 +171,8 @@ async def join(ctx):
     await member.send("Recorded. Up next is your squat 1RM.")
     msg = await bot.wait_for('message', check=check_number)
     while(msg.content.lower() != "yes"):
-        response = msg.content
-        await member.send("You answered \"" + msg.content + "\". Is this correct?")
+        response = str(int(msg.content))
+        await member.send("You answered \"" + response + "\". Is this correct?")
         msg = await bot.wait_for('message', check=check_confirmation)
         if (msg.content.lower() == "no"):
             await member.send("You responded no. Please respond with your squat 1RM.")
@@ -185,8 +185,8 @@ async def join(ctx):
     await member.send("Jotted that one down too. One more, what's your bench press 1RM?")
     msg = await bot.wait_for('message', check=check_number)
     while(msg.content.lower() != "yes"):
-        response = msg.content
-        await member.send("You answered \"" + msg.content + "\". Is this correct?")
+        response = str(int(msg.content))
+        await member.send("You answered \"" + response + "\". Is this correct?")
         msg = await bot.wait_for('message', check=check_confirmation)
         if (msg.content.lower() == "no"):
             await member.send("You responded no. Please respond with your bench press 1RM.")
@@ -194,9 +194,6 @@ async def join(ctx):
 
     bench = response
     f.write(response + "\n")
-
-	# Move on to some more logistical questions. 
-    await member.send("Time to move on to some more logistical questions.")
 
     # Find how many days the users wants to work out. 
     await member.send("This program has three \"versions\" - a four day, five day, and a six day regimen. Which one of these do you want to do?")
@@ -210,41 +207,8 @@ async def join(ctx):
             msg = await bot.wait_for('message', check=check_day)
 
     f.write(response + "\n")
-
-    # Used to store whether they pick automatic or manual. 
-    setting = ""
-			
-	# Ask them if they want the workout to be automatically sent or not. 
-    await member.send("I can send you your program automatically every day at a specified time or you can manually activate the workout. Which do you prefer? Respond \"manual\" or \"automatic\".")
-    msg = await bot.wait_for('message', check=check_setting)
-    while(msg.content.lower() != "yes"):
-        setting = msg.content
-        await member.send("You chose the \"" + msg.content + "\" setting. Is this correct?")
-        msg = await bot.wait_for('message', check=check_confirmation)
-        if (msg.content.lower() == "no"):
-            await member.send("You responded no. Please let me know whether you want me to automatically or manually send you your workout.")
-            msg = await bot.wait_for('message', check=check_setting)
-
-    f.write(setting + "\n")
-
-    # They don't need to do anything if they chose it to be manual. 
-    if (setting.lower() == "manual"):
-        await member.send("Since you chose the manual setting, no further action is required.")
-
-    # If it was automatic, they need to specify a time and whether or not they are working out today (since you can't manually ask for the plan).
-    elif (setting.lower() == "automatic"):
-        await member.send("You chose to have me automatically send you your workout every day. Please provide a time, such as 23:59, for me to so.")
-        msg = await bot.wait_for('message', check=check_time)
-        while(msg.content.lower() != "yes"):
-            response = msg.content
-            await member.send("You chose to workout at " + msg.content + ". Is this correct?")
-            msg = await bot.wait_for('message', check=check_confirmation)
-            if (msg.content.lower() == "no"):
-                await member.send("You responded no. Please tell me what time of the day you want to have your workout sent.")
-                msg = await bot.wait_for('message', check=check_time)
-        f.write(response)
 				
-    await member.send("And we're all set! I'm going to send a couple messages and pin them, but your work here is done. I hope to see you for a workout soon!")
+    await member.send("All set! I hope to see you for a workout soon!")
 
     # Send the user's initial (and in the future current) lifts.
     lifts = """\
@@ -252,12 +216,233 @@ async def join(ctx):
     DEADLIFT - {deadlift}\n\
     OVERHEAD PRESS - {press}\n\
     SQUAT - {squat}\n\
-    BENCH PRESS -{bench}\n\
+    BENCH PRESS - {bench}\
     """.format(deadlift=deadlift, press=press, squat=squat, bench=bench)
     msg = await member.send(lifts)
     await msg.pin()
 
     f.close()
 
+# Start today's workout. 
+@bot.command(name="start")
+async def start(ctx):
+    # First, this command can't be run in either #server-access or #help. I want it to only be usable in DMs, prevents server clog.
+    if (ctx.message.channel.id == 844327265533165590) or (ctx.message.channel.id == 844328208350707712):
+        return
+
+    message = ctx.message
+    channel = ctx.channel
+    member = message.author
+
+    await member.send("Right now, this functionality is missing. Work in progress!")
+
+# Increases a lifts current training max (or TM). 
+@bot.command(name="increase")
+async def increase(ctx, arg1, arg2):
+    # First, this command can't be run in either #server-access or #help. I want it to only be usable in DMs, prevents server clog.
+    if (ctx.message.channel.id == 844327265533165590) or (ctx.message.channel.id == 844328208350707712):
+        return
+
+    message = ctx.message
+    channel = ctx.channel
+    member = message.author
+    channel_id = channel.id
+
+    # Check if the first argument is valid.
+    if arg1 not in ("deadlift", "press", "squat", "bench"):
+        await member.send("Your first argument was invalid. Please try again, using one of these four accepted values: \"deadlift\", \"press\", \"squat\", or \"bench\".")
+        return
+
+    # Find the users ID and open their file. 
+    filename = "Users/" + str(channel_id) + ".txt"
+    f = open(filename, "r")
+
+    # Read the lines and find their lifts. 
+    lines = f.readlines()
+    deadlift = int(lines[1])
+    press = int(lines[2])
+    squat = int(lines[3])
+    bench = int(lines[4])
+
+    # Store the value of the current lift. 
+    selected_lift = ""
+    selected_val = 0
+    if arg1.lower() == "deadlift":
+        selected_lift = "deadlift"
+        selected_val = deadlift
+    elif arg1.lower() == "press":
+        selected_lift = "press"
+        selected_val = press
+    elif arg1.lower() == "squat":
+        selected_lift = "squat"
+        selected_val = squat
+    elif arg1.lower() == "bench":
+        selected_lift = "bench"
+        selected_val = bench
+
+    # Check if the second argument is more valid. More can go wrong here, more complex error checking.
+    if selected_val + int(arg2) > 1200:
+        await member.send("You're either a world-class strongman, a superhero, or you're giving me an invalid number. First two are unlikely, last one is rude. Try again.")
+        return
+
+    elif int(arg2) == 0:
+        await member.send("You're trying to increase your TM by... 0? That doesn't do anything.")
+        return
+
+    elif int(arg2) < 0:
+        await member.send("You're trying to increase your TM by a negative number. Just use !decrease at that point.")
+        return
+
+    old_val = selected_val
+    new_val = 0
+
+    # Now we can modify the lift itself. 
+    if selected_lift == "deadlift":
+        deadlift = deadlift + int(arg2)
+        lines[1] = str(deadlift) + "\n"
+        new_val = deadlift
+    elif selected_lift == "press":
+        press = press + int(arg2)
+        lines[2] = str(press) + "\n"
+        new_val = deadlift
+    elif selected_lift == "squat":
+        squat = squat + int(arg2)
+        lines[3] = str(squat) + "\n"
+        new_val = deadlift
+    elif selected_lift == "bench":
+        bench = bench + int(arg2)
+        lines[4] = str(bench)
+        new_val = deadlift
+
+    # Open the file again, this time to write. Re-make the file with the updated value. 
+    f = open(filename, "w")
+    f.writelines(lines)
+    f.close()
+
+    # Create the string. 
+    lifts = """\
+    Your current lifts are...\n\
+    DEADLIFT - {deadlift}\n\
+    OVERHEAD PRESS - {press}\n\
+    SQUAT - {squat}\n\
+    BENCH PRESS - {bench}\
+    """.format(deadlift=deadlift, press=press, squat=squat, bench=bench)
+
+    # Edit the message. 
+    msg_to_edit = (await channel.pins())[0]
+    await msg_to_edit.edit(content = lifts)
+
+    # Inform the user their lifts have been changed. 
+    updated = """\
+        Updated your {selected_lift} value.\n\
+        Was: {old_val}\n\
+        Now: {new_val}\
+    """.format(selected_lift=selected_lift, old_val=old_val, new_val=new_val)
+    await member.send(updated)
+
+# Decreases a lifts current TM.
+@bot.command(name="decrease")
+async def decrease(ctx, arg1, arg2):
+    # First, this command can't be run in either #server-access or #help. I want it to only be usable in DMs, prevents server clog.
+    if (ctx.message.channel.id == 844327265533165590) or (ctx.message.channel.id == 844328208350707712):
+        return
+
+    message = ctx.message
+    channel = ctx.channel
+    member = message.author
+    channel_id = channel.id
+
+    # Check if the first argument is valid.
+    if arg1 not in ("deadlift", "press", "squat", "bench"):
+        await member.send("Your first argument was invalid. Please try again, using one of these four accepted values: \"deadlift\", \"press\", \"squat\", or \"bench\".")
+        return
+
+    # Find the users ID and open their file. 
+    filename = "Users/" + str(channel_id) + ".txt"
+    f = open(filename, "r")
+
+    # Read the lines and find their lifts. 
+    lines = f.readlines()
+    deadlift = int(lines[1])
+    press = int(lines[2])
+    squat = int(lines[3])
+    bench = int(lines[4])
+
+    # Store the value of the current lift. 
+    selected_lift = ""
+    selected_val = 0
+    if arg1.lower() == "deadlift":
+        selected_lift = "deadlift"
+        selected_val = deadlift
+    elif arg1.lower() == "press":
+        selected_lift = "press"
+        selected_val = press
+    elif arg1.lower() == "squat":
+        selected_lift = "squat"
+        selected_val = squat
+    elif arg1.lower() == "bench":
+        selected_lift = "bench"
+        selected_val = bench
+
+    # Check if the second argument is more valid. More can go wrong here, more complex error checking.
+    if selected_val - int(arg2) <= 0:
+        await member.send("You're attempting to lower your lift below 0. Please try again.")
+        return
+
+    elif int(arg2) == 0:
+        await member.send("You're trying to decrease your TM by... 0? That doesn't do anything.")
+        return
+
+    elif int(arg2) < 0:
+        await member.send("You're trying to decrease your TM by a negative number. Just use !increase at that point.")
+        return
+
+    old_val = selected_val
+    new_val = 0
+
+    # Now we can modify the lift itself. 
+    if selected_lift == "deadlift":
+        deadlift = deadlift - int(arg2)
+        lines[1] = str(deadlift) + "\n"
+        new_val = deadlift
+    elif selected_lift == "press":
+        press = press - int(arg2)
+        lines[2] = str(press) + "\n"
+        new_val = deadlift
+    elif selected_lift == "squat":
+        squat = squat - int(arg2)
+        lines[3] = str(squat) + "\n"
+        new_val = deadlift
+    elif selected_lift == "bench":
+        bench = bench - int(arg2)
+        lines[4] = str(bench)
+        new_val = deadlift
+
+    # Open the file again, this time to write. Re-make the file with the updated value. 
+    f = open(filename, "w")
+    f.writelines(lines)
+    f.close()
+
+    # Create the string. 
+    lifts = """\
+    Your current lifts are...\n\
+    DEADLIFT - {deadlift}\n\
+    OVERHEAD PRESS - {press}\n\
+    SQUAT - {squat}\n\
+    BENCH PRESS - {bench}\
+    """.format(deadlift=deadlift, press=press, squat=squat, bench=bench)
+
+    # Edit the message. 
+    msg_to_edit = (await channel.pins())[0]
+    await msg_to_edit.edit(content = lifts)
+
+    # Inform the user their lifts have been changed. 
+    updated = """\
+        Updated your {selected_lift} value.\n\
+        Was: {old_val}\n\
+        Now: {new_val}\
+    """.format(selected_lift=selected_lift, old_val=old_val, new_val=new_val)
+    await member.send(updated)
+    
 
 bot.run(TOKEN)
